4fb2d90b6f619899b0978e8027e5c35345dd91fa
vo3xel <gh@v3x.xyz>
2021-02-19 10:23:22 +0100

    build sh updated

--- build-nightly-precompiled-deps.sh
+++ build-nightly-precompiled-deps.sh
@@ -6,4 +6,23 @@ BLENDER_VERSION_CYCLE=$(curl -vs https://git.blender.org/gitweb/gitweb.cgi/blend
 
 BLENDER_VERSION_STRING="${BLENDER_MAJOR}.${BLENDER_MINOR}.${BLENDER_VERSION_PATCH}-${BLENDER_VERSION_CYCLE}"
 
+echo "Current blender version: ${BLENDER_VERSION_STRING}"
+
 docker build --build-arg BLENDER_VERSION_STRING="$BLENDER_VERSION_STRING" -t vo3xel/blender-python-module:"$BLENDER_VERSION_STRING" -f nightly-precompiled-deps.Dockerfile .
+
+docker run -v $PWD/data:/home/blender/data vo3xel/blender-python-module:"$BLENDER_VERSION_STRING" test.py
+
+BLENDER_TEST_FILE=./data/my.blend
+if test -f "$BLENDER_TEST_FILE"; then
+    echo "***********************************************************************"
+    echo "Blender python module test OK"
+    echo "***********************************************************************"
+    docker login
+    docker push vo3xel/blender-python-module:"$BLENDER_VERSION_STRING"
+    docker tag vo3xel/blender-python-module:"$BLENDER_VERSION_STRING" vo3xel/blender-python-module:latest
+    docker push vo3xel/blender-python-module:latest
+else
+    echo "***********************************************************************"
+    echo "Blender python module test failed"
+    echo "***********************************************************************"
+fi
