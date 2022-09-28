e68b5ce0931aa0aa636706a88eae78ad05991e9e
Michael Spitzer <michael.spitzer@v2c2.at>
2022-09-28 15:16:28 +0200

    script updated

--- build-release-precompiled-deps.sh
+++ build-release-precompiled-deps.sh
@@ -4,15 +4,17 @@ echo "Current blender version: $1"
 echo "Current precompiled libs version: $2"
 echo "Current blender python version: $3"
 
-rm ./data/my.blend
+BLENDER_TEST_FILE=./data/my.blend
+
+if test -f "$BLENDER_TEST_FILE"; then
+    rm -f "$BLENDER_TEST_FILE"
+fi
 
 docker build --no-cache --build-arg BLENDER_VERSION_STRING="$1" --build-arg BLENDER_PRECOMPILED_LIBS_VERSION_STRING="$2" --build-arg BLENDER_PYTHON_VERSION="$3" -t vo3xel/blender-python-module:"$1"-build-env -f build-env.Dockerfile .
 
 docker build --no-cache --build-arg BLENDER_VERSION_STRING="$1" -t vo3xel/blender-python-module:"$1" -f release-precompiled-deps.Dockerfile .
 
 docker run -v $PWD/data:/home/blender/data vo3xel/blender-python-module:"$1" test.py
-
-BLENDER_TEST_FILE=./data/my.blend
 
 if test -f "$BLENDER_TEST_FILE"; then
     echo "***********************************************************************"
